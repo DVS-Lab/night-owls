2630c874f1708a9d49d5bee5989998c959f10105
David V. Smith <david.v.smith@temple.edu>
2025-03-01 08:28:05 -0500

    initial changes

--- code/gen3colfiles.sh
+++ code/gen3colfiles.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 # this script will convert your BIDS *events.tsv files into the 3-col format for FSL
-# it relies on Tom Nichols' converter, which we store locally under /data/tools
+# it relies on Tom Nichols' converter, which has been copied to our scriptdir to preserve modularity
 # https://github.com/bids-standard/bidsutils
 
-# To do:
-# 0) currently only works for sharedreward following srndna-data model
-# 1) make general for all tasks? not sure that is preferred since task leaders need to be responsible for their tasks
-# 2) add parametric modulators?
-# 3) log missing inputs?
-# 4) zero padding for run number. fix at heudiconv conversion
 
 
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -20,17 +14,19 @@ if [ ! -d ${baseout} ]; then
 fi
 
 sub=$1
+ses=$2
 
+for task in mid sharedreward; do
+  for run in 1 2; do
+    input=${maindir}/bids/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${task}_run-${run}_events.tsv
+    output=${baseout}/sub-${sub}/ses-${ses}/${task}/run-${run}
 
-for acq in mb1me1 mb1me4 mb3me1 mb3me4 mb6me1 mb6me4 mb2me4 mb3me1fa50 mb3me3 mb3me3ip0 mb3me4 mb3me4fa50; do
-  input=${maindir}/bids/sub-${sub}/func/sub-${sub}_task-sharedreward*_acq-${acq}_events.tsv
-  output=${baseout}/sub-${sub}/sharedreward/acq-${acq}
-
-  if [ -e $input ]; then
-  	  mkdir -p $output
-    bash ${scriptdir}/BIDSto3col.sh $input ${output}/
-  else
-    echo "PATH ERROR: cannot locate ${input}."
-    continue
-  fi
+    if [ -e $input ]; then
+    	  mkdir -p $output
+      bash $scriptdir/BIDSto3col.sh $input ${output}/
+    else
+      echo "PATH ERROR: cannot locate ${input}."
+      continue
+    fi
+  done
 done
