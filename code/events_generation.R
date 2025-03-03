cd7363565b098cde9b45cd6c60b41dfe34ca6462
mattmattoni <matt.mattoni@temple.edu>
2025-03-03 11:19:55 -0500

    reorganization 1
    setting folders up, need to fix scripts to accomodate.

---
+++ code/events_generation.R
@@ -0,0 +1,128 @@
+library(readr)
+
+events_gen <- function(sub,ses){
+  sub <- as.character(sub)
+  ses <- as.character(ses)
+  for (r in 1:2){
+    
+    #Reset dataframes
+    mid.events <- sr.events <- as.data.frame(matrix(NA,0,4))
+    
+    
+    #Generate folders, if don't exist
+    out.path <- paste0('C:/Users/mmatt/Desktop/Projects/NightOwls/night-owls/behavioral/sub-',
+                       sub,'/ses-0',ses,'/func/')
+    if (!dir.exists(out.path)) {
+      dir.create(out.path, recursive = TRUE)
+    }
+    
+    
+    #MID
+    
+    raw.mid <- read.csv(paste0('C:/Users/mmatt/Desktop/Projects/NightOwls/night-owls/mid/data/sub-',sub,'/sub-',sub,
+                               '_task-mid_ses-',ses,'_run-',r,'.csv'))
+    ##Trial Timing
+    mid.cue <- cbind(raw.mid$cue_onset_time[-57],raw.mid$cue_duration[-57],rep(NA,56),'n/a')
+    mid.isi <- cbind(raw.mid$isi_onset_time[-57],raw.mid$isi_duration_act[-57],rep(NA,56),'n/a')
+    mid.target <- cbind(raw.mid$target_onset_time[-57],raw.mid$target_duration[-57],rep('target',56),'n/a')
+    mid.target[,4] <- round(raw.mid$Target_Resp.rt[-57],4)
+    mid.target[,4][is.na(mid.target[,4])] <- 'n/a'
+    mid.feedback <- cbind(raw.mid$feedback_onset_time[-57],raw.mid$feedback_duration[-57],rep(NA,56),'n/a')
+    
+    ##Trial Types
+    for (t in 1:56){
+      if (raw.mid$cue.color[t] == 'Green'){
+        mid.cue[t,3] <- 'cue_reward'
+        mid.isi[t,3] <- 'isi_reward'
+        if (raw.mid$.response[t] == 1){
+          mid.feedback[t,3] <- 'feedback_positive_reward'}
+        else {
+          mid.feedback[t,3] <- 'feedback_negative_reward'}
+      }
+      else {
+        mid.cue[t,3] <- 'cue_neutral'
+        mid.isi[t,3] <- 'isi_neutral'
+        if (raw.mid$.response[t] == 1){
+          mid.feedback[t,3] <- 'feedback_positive_neutral'}
+        else {
+          mid.feedback[t,3] <- 'feedback_negative_neutral'}
+      }
+    }
+    
+    ##Merge base events
+    mid.events <- rbind(mid.events,mid.cue,mid.isi,mid.target,mid.feedback)
+    
+    ##Create combined cue regressor
+    mid.antic <- mid.cue
+    mid.antic[,2] <- as.numeric(mid.cue[,2]) + as.numeric(mid.isi[,2])
+    mid.antic[,3][mid.antic[,3]=='cue_reward'] <- 'anticipation_reward'
+    mid.antic[,3][mid.antic[,3]=='cue_neutral'] <- 'anticipation_neutral'
+    
+    ##Merge, Round, Order
+    mid.events <- rbind(mid.events,mid.antic)
+    colnames(mid.events) <- c('onset','duration','trial_type','response_time')
+    mid.events$onset <- round(as.numeric(mid.events$onset),4)
+    mid.events$duration <- round(as.numeric(mid.events$duration),4)
+    mid.events <- mid.events[order(mid.events$onset),]
+    
+    ##Export
+    mid.out <- paste0('sub-',sub,'_ses-0',ses,'_task-mid_run-0',r,'_events.tsv')
+    write_delim(mid.events,paste0(out.path,mid.out))     
+    
+    
+    
+    #Shared Reward
+
+    raw.sr <- read.csv(paste0('C:/Users/mmatt/Desktop/Projects/NightOwls/night-owls/sharedreward/logs/sub-',sub,'/sub-',sub,
+                               '_task-sharedreward_ses-',ses,'_run-',r,'_raw.csv'))
+    ##Trial Timing
+    sr.cue <- cbind(raw.sr$decision_onset,raw.sr$ISI_onset-raw.sr$decision_onset,rep(NA,54),'n/a')
+    sr.feedback <- cbind(raw.sr$outcome_onset,raw.sr$outcome_offset-raw.sr$outcome_onset,rep(NA,54),'n/a')
+    
+    ##Trial Types
+    for (t in 1:54){
+      if (raw.sr$resp[t] == 2 | raw.sr$resp == 3){
+        if (raw.sr$outcome_val[t] == 1){
+          sr.feedback[t,3] <- 'feedback_positive_reward'}
+        #Add all outcome options...
+        else {
+          sr.feedback[t,3] <- 'feedback_negative_reward'}
+      }
+      #If Missed
+      else {
+        sr.cue[t,3] <- 'cue_neutral'
+        sr.isi[t,3] <- 'isi_neutral'
+        if (raw.sr$.response[t] == 1){
+          sr.feedback[t,3] <- 'feedback_positive_neutral'}
+        else {
+          sr.feedback[t,3] <- 'feedback_negative_neutral'}
+      }
+    }
+    
+    ##Merge base events
+    sr.events <- rbind(sr.events,sr.cue,sr.isi,sr.target,sr.feedback)
+    
+    ##Create combined cue regressor
+    sr.antic <- sr.cue
+    sr.antic[,2] <- as.numeric(sr.cue[,2]) + as.numeric(sr.isi[,2])
+    sr.antic[,3][sr.antic[,3]=='cue_reward'] <- 'anticipation_reward'
+    sr.antic[,3][sr.antic[,3]=='cue_neutral'] <- 'anticipation_neutral'
+    
+    ##Merge, Round, Order
+    sr.events <- rbind(sr.events,sr.antic)
+    colnames(sr.events) <- c('onset','duration','trial_type','response_time')
+    sr.events$onset <- round(as.numeric(sr.events$onset),4)
+    sr.events$duration <- round(as.numeric(sr.events$duration),4)
+    sr.events <- sr.events[order(sr.events$onset),]
+    
+    ##Export
+    sr.out <- paste0('sub-',sub,'_ses-0',ses,'_task-sr_run-0',r,'_events.tsv')
+    write_delim(sr.events,paste0(out.path,sr.out))   
+  } 
+}
+
+#Ses-01 COMPLETED
+#events_gen(101,1)
+
+#Ses-02 Completed
+#events_gen(101,02)
