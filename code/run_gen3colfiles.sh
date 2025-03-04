6bf6c9b197912db703bdbeaaa9cd07a62d71ccdc
David V. Smith <david.v.smith@temple.edu>
2025-03-04 00:28:41 -0500

    add loop structure

--- code/run_gen3colfiles.sh
+++ code/run_gen3colfiles.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
 
-for sub in `ls -d ${basedir}/bids/sub-*/`; do
+for subinfo in "101 01" "101 02" "101 03"; do
 
-          sub=${sub#*sub-}
-          sub=${sub%/}
-	bash /ZPOOL/data/projects/multiecho-pilot/code/gen3colfiles.sh $sub
+	# split subinfo variable
+	set -- $subinfo
+	sub=$1
+	ses=$2
+
+	for run in 1 2; do
+
+		script=${scriptdir}/gen3colfiles.sh
+		NCORES=10
+		while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
+			sleep 5s
+		done
+		bash $script $sub $ses &
+		sleep 5s
+	done
+	
 done
