f1088dfb2a81e81ac326e8065893f33533d7ce64
mattmattoni <matt.mattoni@temple.edu>
2025-06-18 11:47:21 -0500

    generate session-by-session fmriprep script

---
+++ code/gen_fmriprep_ses.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# ensure paths are correct
+projectname=night-owls 
+maindir=/gpfs/scratch/tug87422/smithlab-shared/$projectname
+scriptdir=$maindir/code
+bidsdir=$maindir/bids
+logdir=$maindir/logs
+mkdir -p $logdir
+
+#Keep scratch and derivatives dirs separate by sessions
+for ses in {01..12}; do
+  if [ ! -d "$maindir/scratch/ses-$ses" ]; then
+    mkdir -p "$maindir/scratch/ses-$ses"
+  fi
+   if [ ! -d "$maindir/derivatives/ses-$ses" ]; then
+    mkdir -p "$maindir/derivatives/ses-$ses"
+  fi
+done
+
+# Generate configs and PBS scripts
+
+subjects=(sub-101) 
+
+for sub in ${subjects[@]}; do
+	
+    ## for sessions that have a functional image..
+	sessions=($(find "${bidsdir}/${sub}" -type f -path "*/func/*.nii.gz" | grep -o "ses-[^/]*" | sort -u))
+  
+  for ses in "${sessions[@]}"; do
+    sesid=${ses#ses-}
+		
+    ## Create subject/session-specific BIDS filter file
+    configfile=$maindir/code/fmriprep_config_${sub}_${ses}.json
+    cat <<EOF > "$configfile"
+{
+  "t1w": {"datatype": "anat", "suffix": "T1w", "session": "$sesid"},
+  "sbref": {"datatype": "func", "suffix": "sbref", "part": [null, "mag"], "session": "$sesid"},
+  "bold": {"datatype": "func", "suffix": "bold", "part": [null, "mag"], "session": "$sesid"}
+}
+EOF
+
+    # Generate subject/session-specific PBS script
+    scriptfile=$maindir/code/fmriprep_${sub}_${ses}.sh
+
+    cat <<EOF > "$scriptfile"
+#!/bin/bash
+#PBS -l walltime=8:00:00
+#PBS -N fmriprep-${sub}-${ses}
+#PBS -q normal
+#PBS -l nodes=1:ppn=14
+
+# load modules and go to workdir
+module load fsl/6.0.2
+source \$FSLDIR/etc/fslconf/fsl.sh
+module load singularity
+cd \$PBS_O_WORKDIR
+
+# ensure paths are correct
+projectname=night-owls 
+maindir=/gpfs/scratch/tug87422/smithlab-shared/\$projectname
+scriptdir=\$maindir/code
+bidsdir=\$maindir/bids
+logdir=\$maindir/logs
+mkdir -p \$logdir
+
+TEMPLATEFLOW_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/templateflow
+MPLCONFIGDIR_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/mplconfigdir
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
+export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
+
+singularity run --cleanenv \\
+	-B \${TEMPLATEFLOW_DIR}:/opt/templateflow \\
+	-B \${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \\
+	-B \$maindir:/base \\
+	-B /gpfs/scratch/tug87422/smithlab-shared/tools/licenses:/opts \\
+	-B \$maindir/scratch/$ses:/scratch \\
+	/gpfs/scratch/tug87422/smithlab-shared/tools/fmriprep-24.1.1.simg \\
+	/base/bids /base/derivatives/$ses \\
+	participant --participant_label $sub \\
+	--stop-on-first-crash \\
+	--skip-bids-validation \\
+	--nthreads 14 \\
+	--me-output-echos \\
+	--output-spaces MNI152NLin6Asym \\
+	--bids-filter-file /base/code/fmriprep_config_${sub}_${ses}.json \\
+	--fs-no-reconall --fs-license-file /opts/fs_license.txt \\
+	-w /scratch
+EOF
+
+    chmod +x "$scriptfile"
+
+  done
+done
