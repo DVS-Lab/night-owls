9700af928f0fba6a822da2ac5f6ab8635c0a2061
mattmattoni <matt.mattoni@temple.edu>
2025-06-23 12:51:42 -0500

    Update run_warpkit-hpc.sh

--- code/run_warpkit-hpc.sh
+++ code/run_warpkit-hpc.sh
@@ -4,10 +4,16 @@
 maindir=/gpfs/scratch/tug87422/smithlab-shared/night-owls #this should be the only line that has to change if the rest of the script is set up correctly
 scriptdir=$maindir/code
 
+mapfile -t lines < "$scriptdir/sublist.txt"
 pairs=()
-while read -r sub ses; do
+for line in "${lines[@]}"; do
+  # split into sub and ses
+  sub=${line%%[[:space:]]*}
+  ses=${line##*[[:space:]]}
+  [[ -z $sub || -z $ses ]] && continue
   pairs+=( "${sub}:${ses}" )
-done < "$scriptdir/sublist.txt"
+done
+
 
 # join into one space-delimited string
 PAIR_LIST="${pairs[*]}"
