8e0c1c10cbced3cc12673ed20ce553a22f8d2d56
mattmattoni <matt.mattoni@temple.edu>
2025-06-23 12:38:57 -0500

    warpkit inputs sub and ses

--- code/run_warpkit-hpc.sh
+++ code/run_warpkit-hpc.sh
@@ -4,16 +4,13 @@
 maindir=/gpfs/scratch/tug87422/smithlab-shared/night-owls #this should be the only line that has to change if the rest of the script is set up correctly
 scriptdir=$maindir/code
 
+pairs=()
+while read -r sub ses; do
+  pairs+=( "${sub}:${ses}" )
+done < "$scriptdir/sublist.txt"
 
-mapfile -t myArray < ${scriptdir}/sublist.txt
+# join into one space-delimited string
+PAIR_LIST="${pairs[*]}"
 
-
-# grab the first 10 elements
-ntasks=1
-counter=0
-while [ $counter -lt ${#myArray[@]} ]; do
-	subjects=${myArray[@]:$counter:$ntasks}
-	echo $subjects
-	let counter=$counter+$ntasks
-	qsub -v subjects="${subjects[@]}" warpkit-hpc.sh
-done
+echo "Submitting warpkit for: ${PAIR_LIST}"
+qsub -v PAIRS="$PAIR_LIST" warpkit-hpc.sh
--- code/warpkit-hpc.sh
+++ code/warpkit-hpc.sh
@@ -9,6 +9,8 @@ source $FSLDIR/etc/fslconf/fsl.sh
 module load singularity
 cd $PBS_O_WORKDIR
 
+IFS=' ' read -r -a pairs <<< "$PAIRS"
+
 maindir=/gpfs/scratch/tug87422/smithlab-shared/night-owls
 toolsdir=/gpfs/scratch/tug87422/smithlab-shared/tools
 scriptdir=$maindir/code
@@ -25,76 +27,93 @@ MPLCONFIGDIR_DIR=$toolsdir/mplconfigdir
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
 export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
 
-for sub in ${subjects[@]}; do
-	for ses in {01..02}; do
-		for task in "sharedreward" "mid" "rest"; do
-			for run in 1 2; do
+for pair in "${pairs[@]}"; do
+	IFS=':' read -r sub ses <<< "$pair"
+  	echo "Processing sub-${sub}, ses-${ses}"
 
-					rm -f $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
-					touch $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
-		
-					outdir=$maindir/derivatives/warpkit/sub-${sub}/ses-${ses}
-					if [ ! -d $outdir ]; then
-						mkdir -p $outdir
-					fi
+	compiled_jobs="$logdir/cmd_warpkit_sub-${sub}_ses-${ses}_compiled.txt"
+	rm -f $compiled_jobs
 
-				fmapdir=$maindir/bids/sub-${sub}/ses-${ses}/fmap
-				if [ ! -d $fmapdir ]; then
-							mkdir -p $fmapdir
-						fi
-		
-					indir=${maindir}/bids/sub-${sub}/ses-${ses}/func
-		
-					if [ ! -e $indir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json ]; then
-						echo "NO DATA: sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json"
-						echo "NO DATA: sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json" >> $scriptdir/missingFiles-warpkit.log
-						continue
-					fi
-		
-					# don't re-do existing output
-					if [ -e $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap.nii.gz ]; then
-						echo "EXISTS (skipping): sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap.nii.gz"
-						continue
-					fi
-		
-					# delete default gre fmaps (phasediff)
-					if [ -e $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-bold_phasediff.nii.gz ]; then
-						rm -rf $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-bold*
-					fi
-		
-					echo "singularity run --cleanenv \
-					-B $indir:/base \
-					-B $outdir:/out \
-					$toolsdir/warpkit.sif \
-					--magnitude /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.nii.gz \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-mag_bold.nii.gz \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-mag_bold.nii.gz \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-mag_bold.nii.gz \
-					--phase /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-phase_bold.nii.gz \
-						/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-phase_bold.nii.gz \
-						/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-phase_bold.nii.gz \
-						/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-phase_bold.nii.gz \
-					--metadata /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-phase_bold.json \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-phase_bold.json \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-phase_bold.json \
-							/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-phase_bold.json \
-					--out_prefix /out/sub-${sub}_ses-${ses}_task-${task}_run-${run}" >> $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
-		
-				torque-launch -p $logdir/chk_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
-		
-				# extract first volume as fieldmap and copy to fmap dir. still need json files for these.
+	for task in "sharedreward" "mid" "rest"; do
+		for run in 1 2; do
+
+			rm -f $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
+			touch $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_${task}_${run}.txt
+
+			outdir=$maindir/derivatives/warpkit/sub-${sub}/ses-${ses}
+			if [ ! -d $outdir ]; then
+				mkdir -p $outdir
+			fi
+
+			fmapdir=$maindir/bids/sub-${sub}/ses-${ses}/fmap
+			if [ ! -d $fmapdir ]; then
+				mkdir -p $fmapdir
+			fi
+	
+			indir=${maindir}/bids/sub-${sub}/ses-${ses}/func
+	
+			if [ ! -e $indir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json ]; then
+				echo "NO DATA: sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json"
+				echo "NO DATA: sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json" >> $scriptdir/missingFiles-warpkit.log
+				continue
+			fi
+	
+			# don't re-do existing output
+			if [ -e $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap.nii.gz ]; then
+				echo "EXISTS (skipping): sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap.nii.gz"
+				continue
+			fi
+
+			# delete default gre fmaps (phasediff)
+			if [ -e $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-bold_phasediff.nii.gz ]; then
+				rm -rf $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-bold*
+			fi
+	
+			echo "singularity run --cleanenv \
+-B $indir:/base \
+-B $outdir:/out \
+$toolsdir/warpkit.sif \
+--magnitude /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-mag_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-mag_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-mag_bold.nii.gz \
+--phase /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-phase_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-phase_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-phase_bold.nii.gz \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-phase_bold.nii.gz \
+--metadata /base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-phase_bold.json \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-2_part-phase_bold.json \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-3_part-phase_bold.json \
+/base/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-4_part-phase_bold.json \
+--out_prefix /out/sub-${sub}_ses-${ses}_task-${task}_run-${run}" >> $compiled_jobs
+			echo "" >> $compiled_jobs
+
+		done
+	done
+	
+	torque-launch -p $logdir/chk_warpkit_sub-${sub}_ses-${ses}.txt $logdir/cmd_warpkit_sub-${sub}_ses-${ses}_compiled.txt
+
+	# Post-processing for this subject (after all jobs complete)
+	for task in "sharedreward" "mid" "rest"; do
+		for run in 1 2; do
+			outdir=$maindir/derivatives/warpkit/sub-${sub}/ses-${ses}
+			indir=${maindir}/bids/sub-${sub}/ses-${ses}/func
+			
+			# Check if the warpkit output exists before processing
+			if [ -e $outdir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_fieldmaps.nii ]; then
+				# extract first volume as fieldmap and copy to fmap dir
 				fslroi $outdir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_fieldmaps.nii $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap 0 1
 				fslroi $indir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.nii.gz $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_magnitude 0 1
-	
-				# placeholders for json files. will need editing.
+
+				# placeholders for json files
 				cp $indir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-mag_bold.json $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_magnitude.json
 				cp $indir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_part-phase_bold.json $maindir/bids/sub-${sub}/ses-${ses}/fmap/sub-${sub}_ses-${ses}_acq-${task}_run-${run}_fieldmap.json
-	
+
 				# trash the rest
 				rm -rf $outdir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_displacementmaps.nii
 				rm -rf $outdir/sub-${sub}_ses-${ses}_task-${task}_run-${run}_fieldmaps_native.nii
-
-			done
-		done
+			fi
+    	done
 	done
 done
+
