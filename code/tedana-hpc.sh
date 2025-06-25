3f52492a5950b0d591fe7d79ba513006b1f4545c
mattmattoni <matt.mattoni@temple.edu>
2025-06-25 10:32:40 -0500

    Update tedana-hpc.sh

--- code/tedana-hpc.sh
+++ code/tedana-hpc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
-#PBS -N tedana-test
+#PBS -N tedana
 #PBS -q normal
-#PBS -l nodes=12:ppn=7
+#PBS -l nodes=1:ppn=28
 
 # load modules and go to workdir
 module load fsl/6.0.2
@@ -23,19 +23,21 @@ touch $logdir/cmd_tedana_${PBS_JOBID}.txt
 # estimated procs per subject: 4 tasks * 2 runs * 4 echoes = 32?
 # wow... can't be right, but let's go for 9 subjects per job (84 procs per job). watch for memory issues.
 for sub in ${subjects[@]}; do
-	for task in mid sharedreward rest; do
-		for run in 1 2; do
+	for ses in ${01..12};
+		for task in mid sharedreward rest; do
+			for run in 1 2; do
 
-			indata=$maindir/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_task-${task}_run-${run}_echo-1_desc-preproc_bold.nii.gz
-			if [ -e $indata ]; then
-				echo python $scriptdir/tedana-single.py \
-				--fmriprepDir $maindir/derivatives/fmriprep \
-				--bidsDir $bidsdir \
-				--sub $sub \
-				--task $task \
-				--runnum $run >> $logdir/cmd_tedana_${PBS_JOBID}.txt
-			fi
+				indata=$maindir/derivatives/fmriprep/sub-${sub}/func/sub-${sub}_ses-${ses}_task-${task}_run-${run}_echo-1_desc-preproc_bold.nii.gz
+				if [ -e $indata ]; then
+					echo python $scriptdir/tedana-single.py \
+					--fmriprepDir $maindir/derivatives/fmriprep \
+					--bidsDir $bidsdir \
+					--sub $sub \
+					--task $task \
+					--runnum $run >> $logdir/cmd_tedana_${PBS_JOBID}.txt
+				fi
 
+			done
 		done
 	done
 done
