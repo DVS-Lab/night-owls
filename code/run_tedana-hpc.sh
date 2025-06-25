e8365eedc686e9b82b286e2b4dc2036b11d953cf
mattmattoni <matt.mattoni@temple.edu>
2025-06-25 10:25:47 -0500

    adding tedana, specifying sublist names

---
+++ code/run_tedana-hpc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ensure paths are correct
+maindir=/gpfs/scratch/tug47822/night-owls #this should be the only line that has to change if the rest of the script is set up correctly
+scriptdir=$maindir/code
+
+
+mapfile -t myArray < ${scriptdir}/sublist.txt
+
+
+ntasks=9
+counter=0
+while [ $counter -lt ${#myArray[@]} ]; do
+	subjects=${myArray[@]:$counter:$ntasks}
+	echo $subjects
+	let counter=$counter+$ntasks
+	qsub -v subjects="${subjects[@]}" tedana-hpc.sh
+done
