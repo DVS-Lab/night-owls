2d93cd45ae09087974d7cec90c12ffd0dd2b6aa6
mattmattoni <matt.mattoni@temple.edu>
2025-07-11 12:37:19 -0500

    anat-only pipeline

---
+++ code/fmriprep-hpc-anat.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#PBS -l walltime=8:00:00
+#PBS -N fmriprep-nightowls
+#PBS -q large
+#PBS -l nodes=1:ppn=14
+
+# load modules and go to workdir
+module load fsl/6.0.2
+source $FSLDIR/etc/fslconf/fsl.sh
+module load singularity
+cd $PBS_O_WORKDIR
+
+# ensure paths are correct
+projectname=night-owls #this should be the only line that has to change if the rest of the script is set up correctly
+maindir=/gpfs/scratch/tug87422/smithlab-shared/$projectname
+scriptdir=$maindir/code
+bidsdir=$maindir/bids
+logdir=$maindir/logs
+mkdir -p $logdir
+
+#subjects=("${!1}")
+
+rm -f $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+touch $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+
+# make derivatives folder if it doesn't exist.
+# let's keep this out of bids for now
+if [ ! -d $maindir/derivatives/anat-only ]; then
+	mkdir -p $maindir/derivatives
+fi
+
+scratchdir=~/scratch/$projectname/fmriprep
+if [ ! -d $scratchdir ]; then
+	mkdir -p $scratchdir
+fi
+
+TEMPLATEFLOW_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/templateflow
+MPLCONFIGDIR_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/mplconfigdir
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
+export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
+
+for sub in ${subjects[@]}; do
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B /gpfs/scratch/tug87422/smithlab-shared/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		/gpfs/scratch/tug87422/smithlab-shared/tools/fmriprep-24.1.1.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--skip-bids-validation \
+		--nthreads 14 \
+		--me-output-echos \
+		--output-spaces anat MNI152NLin6Asym \
+        --anat-only \
+        --longitudinal \
+		--fs-no-reconall --fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+done
+torque-launch -p $logdir/chk_fmriprep_${PBS_JOBID}.txt $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+
+# --cifti-output 91k \
+# --output-spaces fsLR fsaverage MNI152NLin6Asym \
---
+++ code/run_fmriprep-hpc-anat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ensure paths are correct
+maindir=/gpfs/scratch/tug87422/smithlab-shared/night-owls #this should be the only line that has to change if the rest of the script is set up correctly
+scriptdir=$maindir/code
+
+
+mapfile -t myArray < ${scriptdir}/sublist.txt
+
+
+# grab the first 10 elements
+ntasks=2
+counter=0
+while [ $counter -lt ${#myArray[@]} ]; do
+	subjects=${myArray[@]:$counter:$ntasks}
+	echo $subjects
+	let counter=$counter+$ntasks
+	qsub -v subjects="${subjects[@]}" fmriprep-hpc.sh
+done
