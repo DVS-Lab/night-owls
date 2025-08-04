3f8d63f123ffee654763d07cb58dedff1fad5aec
mattmattoni <matt.mattoni@temple.edu>
2025-08-04 08:56:58 -0500

    change L1 inputs for smoothing

--- code/L1stats-mid.sh
+++ code/L1stats-mid.sh
@@ -18,7 +18,7 @@ model=1 # everyone should just have one model
 # set inputs and general outputs (should not need to change across studies in Smith Lab)
 MAINOUTPUT=${maindir}/derivatives/fsl/space-mni/sub-${sub}/ses-${ses}
 mkdir -p $MAINOUTPUT
-DATA=${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz
+DATA=${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-MNI152NLin6Asym_res-2_desc-preproc_bold_5mm.nii.gz
 if [ ! -e $DATA ]; then
 	echo " Exiting -- missing data: ${DATA}"
 	exit
