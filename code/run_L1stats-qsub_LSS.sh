dfd9f2834604daab742b090aa5dd9f3ec3b7a3cd
ShenghanWang <112357880+ShenghanWang@users.noreply.github.com>
2025-08-06 13:01:07 -0400

    test run 101-ses3-run2-trial2

--- code/run_L1stats-qsub_LSS.sh
+++ code/run_L1stats-qsub_LSS.sh
@@ -12,11 +12,12 @@ counter=0
 
 #still decide to manually add the sub-ses pair because of some sub has different number of sessions
 #add total session number of each p in 
-#for subinfo in "101 01" "101 02" "101 03" "101 04" "101 05" "101 06" "101 07" "101 08" "101 09" "101 10" "101 11"\
-# "103 01" "103 02" "103 03" "103 04" "103 05" "103 06" "103 07" "103 08" "103 09" "103 10" "103 11" "103 12"\
+#for subinfo in "101 01" "101 02" "101 03" "101 04" "101 05" "101 06" "101 07" "101 08" "101 09" "101 10" "101 11"; do  #\
+	# "103 01" "103 02" "103 03" "103 04" "103 05" "103 06" "103 07" "103 08" "103 09" "103 10" "103 11" "103 12"\
  #"104 01" "104 02" "104 03" "104 04" "104 05" "104 06" "104 07" "104 08" "104 09" "104 10" "104 11" "104 12"; do 
  #"105 01" "105 02" "105 03" "105 04" "105 05" "105 06" "105 07" "105 08" "105 09" "105 10" "105 11" "105 12"
-for subinfo in "101 01";do #test
+
+for subinfo in "101 03";do #test
 # set is not working properly in seperating subinfo
 	# split subinfo variable
 	set -- $subinfo
@@ -36,7 +37,7 @@ for subinfo in "101 01";do #test
 				#myArray=($(seq 1 $trial))
 
 				#for trial in $(seq 1 $t); do
-				for trial in 1; do #test
+				for trial in 2; do #test
 				#while [ $counter -lt ${#myArray[@]} ]; do
 				#trial=${myArray[@]:$counter:$ntasks}
 				
