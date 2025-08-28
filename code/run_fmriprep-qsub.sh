ace7c4ce8e0d0e0425ded598e32ee5b19e17b65b
mattmattoni <matt.mattoni@temple.edu>
2025-08-28 18:49:48 -0500

    Update run_fmriprep-qsub.sh

--- code/run_fmriprep-qsub.sh
+++ code/run_fmriprep-qsub.sh
@@ -3,7 +3,8 @@
 
 umask 0000
 
-for sub in 101 103 104 105; do
+#for sub in 101 103 104 105; do
+for sub in 104; do
   for ses in $(seq -w 01 12); do
     qsub fmriprep-anat/fmriprep_sub-${sub}_ses-${ses}.sh
   done
