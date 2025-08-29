54bd902adf1a39d1446bc30a029c44cf42f8acdf
mattmattoni <matt.mattoni@temple.edu>
2025-08-29 14:08:05 -0500

    Update removeAcquisitionDuration.py

--- code/removeAcquisitionDuration.py
+++ code/removeAcquisitionDuration.py
@@ -1,33 +1,32 @@
 import json
 import os
 
-# Test directory
-playdir = "/ZPOOL/data/projects/night-owls/"
+# Define paths
+bidsdir = "/ZPOOL/data/projects/night-owls/bids/"
 
-# Only run for sub-test
-subj = "sub-test"
-subj_path = os.path.join(playdir, subj)
+# Find all subject directories in the BIDS directory
+subs = [d for d in os.listdir(bidsdir) if os.path.isdir(os.path.join(bidsdir, d)) and d.startswith('sub')]
 
-print("Running subject:", subj)
+for subj in subs:
+    print("Running subject:", subj)
 
-# Walk through ses-xx subdirs
-for root, _, files in os.walk(subj_path):
-    if root.endswith('func'):
-        json_files = [f for f in files if f.endswith('bold.json')]
+    for root, _, files in os.walk(os.path.join(bidsdir, subj)):
+        if root.endswith('func'):
+            json_files = [f for f in files if f.endswith('bold.json')]
 
-        for json_file in json_files:
-            json_path = os.path.join(root, json_file)
+            for json_file in json_files:
+                json_path = os.path.join(root, json_file)
 
-            with open(json_path, 'r') as f:
-                data = json.load(f)
+                with open(json_path, 'r') as f:
+                    data = json.load(f)
 
-            # Remove AcquisitionDuration if present
-            if "AcquisitionDuration" in data:
-                del data["AcquisitionDuration"]
+                # Remove AcquisitionDuration if present
+                if "AcquisitionDuration" in data:
+                    del data["AcquisitionDuration"]
 
-                with open(json_path, 'w') as f:
-                    json.dump(data, f, indent=4, sort_keys=True)
+                    with open(json_path, 'w') as f:
+                        json.dump(data, f, indent=4, sort_keys=True)
 
-                print("Removed AcquisitionDuration from", json_path)
-            else:
-                print("No AcquisitionDuration in", json_path)
+                    print("Removed AcquisitionDuration from", json_path)
+                else:
+                    print("No AcquisitionDuration in", json_path)
