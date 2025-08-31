2353b2ab3a55f8e572dcf12a42083590842d6046
David V. Smith <david.v.smith@temple.edu>
2025-08-30 22:31:46 -0400

    restrict to sub-104 and increase walltime

--- code/run_L1stats-qsub_LSS.sh
+++ code/run_L1stats-qsub_LSS.sh
@@ -12,7 +12,8 @@ declare -A SKIP=(
   ["103:12"]=1
 )
 
-for sub in 101 103 104 105; do
+#for sub in 101 103 104 105; do
+for sub in 104; do
   for acq in multiecho single; do
     for confounds in tedana base; do
       for task in mid sharedreward; do
