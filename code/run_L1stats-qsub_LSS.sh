f2145ee7e78191992eff03bc6c1ef17bf2aa17a3
David V. Smith <david.v.smith@temple.edu>
2025-08-31 10:58:55 -0400

    swtich to sub-101

--- code/run_L1stats-qsub_LSS.sh
+++ code/run_L1stats-qsub_LSS.sh
@@ -13,7 +13,7 @@ declare -A SKIP=(
 )
 
 #for sub in 101 103 104 105; do
-for sub in 104; do
+for sub in 101; do
   for acq in multiecho single; do
     for confounds in tedana base; do
       for task in mid sharedreward; do
