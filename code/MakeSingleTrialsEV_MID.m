e0b80b8f3f4cb6a3ee3944a7969d082a90a0f07a
David V. Smith <david.v.smith@temple.edu>
2025-08-30 20:59:23 -0400

    clean up code and run all sessions

--- code/MakeSingleTrialsEV_MID.m
+++ code/MakeSingleTrialsEV_MID.m
@@ -1,30 +1,29 @@
 clear; close all;
 
-% set up dirs
-codedir = pwd; % must run from code, so this is not a good solution
-cd ..
-maindir = pwd;
-%task='mid'
-evdir = fullfile(maindir,'derivatives','fsl','EVFiles');
+% Resolve paths relative to THIS script
+codedir = fileparts(mfilename('fullpath'));   % folder containing this .m file
+maindir = fileparts(codedir);                 % go up one level (adjust if needed)
+evdir   = fullfile(maindir,'derivatives','fsl','EVFiles');
+
 
 % load sub/run list
-sub = [101 103 104];
-session=[9 12 12];
-runs=2;
-log={}
+sub = [101 103 104 105];
+session = [11 12 12 12];
+runs = 2;
+
 
 for s = 1:length(sub)
     for ses=1:session(s)
         sesname=sprintf('ses-%02d', ses);
         for r = 1:runs
-         rundir = fullfile(evdir,['sub-' num2str(sub(s))],sesname,'mid',['run-' num2str(r)]);
-        
-         if ~exist(rundir, 'dir')
-            subchar=num2str(s);
-            seschar=num2str(sesname);
-            log{end+1}=sprintf('sub %s %s run %s does not exist.',num2str(sub(s)),sesname,num2str(r));
-            continue;
-         end
+            rundir = fullfile(evdir,['sub-' num2str(sub(s))],sesname,'mid',['run-' num2str(r)]);
+
+            if ~exist(rundir, 'dir')
+                subchar=num2str(s);
+                seschar=num2str(sesname);
+                fprintf('sub %s %s run %s does not exist.\n',num2str(sub(s)),sesname,num2str(r));
+                continue;
+            end
 
             % load evs and concatenate
             ev1=load(fullfile(rundir,'_anticipation_neutral.txt'));
@@ -32,12 +31,11 @@ for s = 1:length(sub)
             all_evs = [ev1; ev2];
             all_evs = sortrows(all_evs,1,'ascend');
 
-    
+
 
             % check length of trials. everyone should have 56
             if length(all_evs) ~= 56
-                disp(sprintf('sub %s %s run %s missing trials...', num2str(sub(s)), num2str(sesname), num2str(r)));
-                keyboard
+                fprintf('sub %s %s run %s missing trials...\n', num2str(sub(s)), num2str(sesname), num2str(r));
             end
 
             % extract trials and write evs
@@ -52,16 +50,12 @@ for s = 1:length(sub)
 
                 % write out single trial
                 fname = sprintf('run-%d_SingleTrial%02d.txt',r,t);
-                dlmwrite(fullfile(outdir,fname),singletrial,'delimiter','\t','precision','%.6f')
+                writematrix(round(singletrial, 2), fullfile(outdir, fname), 'Delimiter','tab');
 
                 % write out other trials
                 fname = sprintf('run-%d_OtherTrials%02d.txt',r,t);
-                dlmwrite(fullfile(outdir,fname),othertrials,'delimiter','\t','precision','%.6f')
+                writematrix(round(othertrials, 2), fullfile(outdir, fname), 'Delimiter','tab');
             end
         end
     end
 end
-cd(codedir);
-
-
-
--- code/MakeSingleTrialsEV_SR.m
+++ code/MakeSingleTrialsEV_SR.m
@@ -1,18 +1,16 @@
 clear; close all;
 
-% set up dirs
-codedir = pwd; % must run from code, so this is not a good solution
-cd ..
-maindir = pwd;
-%task='sharedreward';
-evdir = fullfile(maindir,'derivatives','fsl','EVFiles');
+% Resolve paths relative to THIS script
+codedir = fileparts(mfilename('fullpath'));   % folder containing this .m file
+maindir = fileparts(codedir);                 % go up one level (adjust if needed)
+evdir   = fullfile(maindir,'derivatives','fsl','EVFiles');
 
 
 % load sub/run list
-sub = [101 103 104];%105
-session=[9 12 12]; 
-runs=2;
-log={}
+sub = [101 103 104 105];
+session = [11 12 12 12];
+runs = 2;
+
 
 for s = 1:length(sub)
     for ses=1:session(s)
@@ -23,46 +21,41 @@ for s = 1:length(sub)
             if ~exist(rundir, 'dir')
                 subchar=num2str(s);
                 seschar=num2str(ses);
-                log{end+1}=sprintf('sub %s %s run %s does not exist.',num2str(sub(s)),sesname,num2str(r));
+                fprintf('sub %s %s run %s does not exist.\n',num2str(sub(s)),sesname,num2str(r));
                 continue;
             end
 
             % load evs and concatenate
             misspath = fullfile(rundir,"_miss_outcome.txt");
 
-        if exist(misspath, 'file')==2
-            ev1=load(fullfile(rundir,'_outcome_computer_neutral.txt'));
-            ev2=load(fullfile(rundir,'_outcome_computer_punish.txt'));
-            ev3=load(fullfile(rundir,'_outcome_computer_reward.txt'));
-            ev4=load(fullfile(rundir,'_outcome_stranger_neutral.txt'));
-            ev5=load(fullfile(rundir,'_outcome_stranger_punish.txt'));
-            ev6=load(fullfile(rundir,'_outcome_stranger_reward.txt'));
-            ev7=load(fullfile(rundir,'_miss_outcome.txt'));
+            if exist(misspath, 'file')==2
+                ev1=load(fullfile(rundir,'_outcome_computer_neutral.txt'));
+                ev2=load(fullfile(rundir,'_outcome_computer_punish.txt'));
+                ev3=load(fullfile(rundir,'_outcome_computer_reward.txt'));
+                ev4=load(fullfile(rundir,'_outcome_stranger_neutral.txt'));
+                ev5=load(fullfile(rundir,'_outcome_stranger_punish.txt'));
+                ev6=load(fullfile(rundir,'_outcome_stranger_reward.txt'));
+                ev7=load(fullfile(rundir,'_miss_outcome.txt'));
 
-            all_evs = [ev1; ev2; ev3; ev4; ev5; ev6;ev7];
-            all_evs = sortrows(all_evs,1,'ascend');
- 
-            [~, miss_idx] = ismember(ev7, all_evs, 'rows');
-            fprintf('sub %s %s run %s missed trial number: %s\n', num2str(sub(s)), sesname, num2str(r), mat2str(miss_idx(miss_idx > 0)))
-%%%%%%take note of which trial has missing outcome event and add into run
-%%%%%%script as the trial that gets skipped(continued)
-        else
-        
-            ev1=load(fullfile(rundir,'_outcome_computer_neutral.txt'));
-            ev2=load(fullfile(rundir,'_outcome_computer_punish.txt'));
-            ev3=load(fullfile(rundir,'_outcome_computer_reward.txt'));
-            ev4=load(fullfile(rundir,'_outcome_stranger_neutral.txt'));
-            ev5=load(fullfile(rundir,'_outcome_stranger_punish.txt'));
-            ev6=load(fullfile(rundir,'_outcome_stranger_reward.txt'));
+                all_evs = [ev1; ev2; ev3; ev4; ev5; ev6; ev7];
+                all_evs = sortrows(all_evs,1,'ascend');
+
+            else
+
+                ev1=load(fullfile(rundir,'_outcome_computer_neutral.txt'));
+                ev2=load(fullfile(rundir,'_outcome_computer_punish.txt'));
+                ev3=load(fullfile(rundir,'_outcome_computer_reward.txt'));
+                ev4=load(fullfile(rundir,'_outcome_stranger_neutral.txt'));
+                ev5=load(fullfile(rundir,'_outcome_stranger_punish.txt'));
+                ev6=load(fullfile(rundir,'_outcome_stranger_reward.txt'));
 
 
-            all_evs = [ev1; ev2; ev3; ev4; ev5; ev6];
-            all_evs = sortrows(all_evs,1,'ascend');
-        end
-            % check length of trials. everyone should have 64
+                all_evs = [ev1; ev2; ev3; ev4; ev5; ev6];
+                all_evs = sortrows(all_evs,1,'ascend');
+            end
+            % check length of trials. everyone should have 54
             if length(all_evs) ~= 54
-                disp(sprintf('CHECK: sub %s %s run %s missing trials even after missed outcomes counted', num2str(sub(s)), sesname, num2str(r)))
-                keyboard
+                fprintf('CHECK: %s %s run %s missing trials even after missed outcomes counted\n', num2str(sub(s)), sesname, num2str(r))
             end
 
             % extract trials and write evs
@@ -79,16 +72,13 @@ for s = 1:length(sub)
 
                 % write out single trial
                 fname = sprintf('run-%d_SingleTrial%02d.txt',r,t);
-                dlmwrite(fullfile(outdir,fname),singletrial,'delimiter','\t','precision','%.6f')
+                writematrix(round(singletrial, 2), fullfile(outdir, fname), 'Delimiter','tab');
 
                 % write out other trials
                 fname = sprintf('run-%d_OtherTrials%02d.txt',r,t);
-                dlmwrite(fullfile(outdir,fname),othertrials,'delimiter','\t','precision','%.6f')
+                writematrix(round(othertrials, 2), fullfile(outdir, fname), 'Delimiter','tab');
+
             end
         end
     end
 end
-cd(codedir);
-
-
-
