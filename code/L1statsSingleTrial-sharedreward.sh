8a1973afd6dc5df3b246f918390ee5eb049e7b6a
David V. Smith <david.v.smith@temple.edu>
2025-09-03 09:13:24 -0400

    don't delete intermediate files

--- code/L1statsSingleTrial-sharedreward.sh
+++ code/L1statsSingleTrial-sharedreward.sh
@@ -103,5 +103,5 @@ feat $OTEMPLATE
 
 # copy zstat image to common output folder and delete feat output
 cp ${OUTPUT}.feat/stats/zstat1.nii.gz ${zoutdir}/zstat_trial-${trial}.nii.gz
-rm -rf ${OUTPUT}.feat ${OTEMPLATE}
+#rm -rf ${OUTPUT}.feat ${OTEMPLATE}
 exit
