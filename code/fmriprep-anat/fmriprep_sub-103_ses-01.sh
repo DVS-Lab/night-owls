5962a4f11861a0aa9bc62b4aaefd1af8687e6d33
David V. Smith <david.v.smith@temple.edu>
2025-09-05 16:53:58 -0400

    remove bad run

--- code/fmriprep-anat/fmriprep_sub-103_ses-01.sh
+++ code/fmriprep-anat/fmriprep_sub-103_ses-01.sh
@@ -36,7 +36,7 @@ singularity run --cleanenv \
 	--skip-bids-validation \
 	--nthreads 14 \
 	--me-output-echos \
-	--output-spaces anat:res-2 MNI152NLin6Asym:res-2 \
+	--output-spaces anat MNI152NLin6Asym \
   --derivatives /gpfs/scratch/tug87422/smithlab-shared/night-owls/derivatives/anat-only \
 	--bids-filter-file /base/code/fmriprep-anat/fmriprep_config_sub-103_ses-01.json \
 	--fs-no-reconall --fs-license-file /opts/fs_license.txt \
