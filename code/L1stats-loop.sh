c5c02470a6cb7f3e9a72f0635df83fc8fef8b224
mattmattoni <matt.mattoni@temple.edu>
2025-12-05 10:44:18 -0600

    Update L1stats-loop.sh

--- code/L1stats-loop.sh
+++ code/L1stats-loop.sh
@@ -17,7 +17,7 @@ td=0 # 1 for on, 0 for off (temporal derivatives)
 model=1 # everyone should just have one model
 
 tasks=(mid sharedreward)
-spaces=(mni t1w)
+spaces=(mni)
 echos=(single-echo multi-echo)
 confounds=(cnfds-fmriprep cnfds-tedana)
 
@@ -35,13 +35,13 @@ for TASK in "${tasks[@]}"; do #Will need echo-2 loop as well
                 mkdir -p $MAINOUTPUT
 
                 if [[ "${space}" == "mni" && "${echo}" == "multi-echo" ]]; then
-                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-MNI152NLin6Asym_desc-preproc_bold_5mm.nii.gz"
+                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz"
                 elif [[ "${space}" == "t1w" && "${echo}" == "multi-echo" ]]; then
-                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-T1w_desc-preproc_bold_5mm.nii.gz"
+                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_part-mag_space-T1w_desc-preproc_bold.nii.gz"
                 elif [[ "${space}" == "mni" && "${echo}" == "single-echo" ]]; then
-                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_echo-2_part-mag_space-MNI152NLin6Asym_desc-preproc_bold_5mm.nii.gz"
+                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_echo-2_part-mag_space-MNI152NLin6Asym_desc-preproc_bold.nii.gz"
                 elif [[ "${space}" == "t1w" && "${echo}" == "single-echo" ]]; then
-                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_echo-2_part-mag_space-T1w_desc-preproc_bold_5mm.nii.gz"
+                    DATA="${maindir}/derivatives/fmriprep/sub-${sub}/ses-${ses}/func/sub-${sub}_ses-${ses}_task-${TASK}_run-${run}_echo-2_part-mag_space-T1w_desc-preproc_bold.nii.gz"
                 else
                     echo "ERROR: unexpected space='$space' (expected 'mni' or 't1w')" >> ${logdir}/sub-${sub}_ses-${ses}_re-runL1.log
                     exit
@@ -81,7 +81,7 @@ for TASK in "${tasks[@]}"; do #Will need echo-2 loop as well
                     ITEMPLATE=${maindir}/templates/L1_task-${TASK}_model-${model}_type-${TYPE}_td.fsf
                     OTEMPLATE=${MAINOUTPUT}/L1_task-${TASK}_model-${model}_type-${TYPE}_run-${run}_td.fsf
                 elif [ $td -eq 0 ]; then
-                    OUTPUT=${MAINOUTPUT}/L1_sub-${sub}_ses-${ses}_task-${TASK}_model-${model}_type-${TYPE}_run-${run}_space-${space}_${echo}_${confound}
+                    OUTPUT=${MAINOUTPUT}/L1_sub-${sub}_ses-${ses}_task-${TASK}_model-${model}_type-${TYPE}_run-${run}_space-${space}_${echo}_${confound}_unsmoothed
                     ITEMPLATE=${maindir}/templates/L1_task-${TASK}_model-${model}_type-${TYPE}.fsf
                     OTEMPLATE=${MAINOUTPUT}/L1_sub-${sub}_task-${TASK}_model-${model}_type-${TYPE}_ses-${ses}_run-${run}_space-${space}_${echo}_${confound}.fsf
                 else
