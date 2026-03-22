1b1cc6b064ce27d8928d49532aa4eeaa559727bd
mattmattoni <matt.mattoni@temple.edu>
2026-03-22 17:42:17 -0500

    Update sub-103_mriqc.sh

--- code/sub-103_mriqc.sh
+++ code/sub-103_mriqc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -l walltime=8:00:00
-#PBS -N mriqc103test
+#PBS -N mriqc103
 #PBS -q normal
 #PBS -l nodes=1:ppn=28
 #PBS -l mem=100gb
-#PBS -o /home/tun47039/mriqc_103.o
-#PBS -e /home/tun47039/mriqc_103.e
+#PBS -o /gpfs/scratch/tun47039/night-owls/logs/mriqc_103.o
+#PBS -e /gpfs/scratch/tun47039/night-owls/logs/mriqc_103.e
 
 module load fsl/6.0.2
 source $FSLDIR/etc/fslconf/fsl.sh
@@ -14,31 +14,26 @@ cd $PBS_O_WORKDIR
 
 umask 0000
 
+# New base directory
+mydir=/gpfs/scratch/tun47039/night-owls
 dsroot=/gpfs/scratch/tug87422/smithlab-shared/night-owls
-codedir=$dsroot/code
-logdir=~/work/logs
-mkdir -p $logdir
+
+# Create necessary directories
+mkdir -p $mydir/logs
+mkdir -p $mydir/scratch
+mkdir -p $mydir/derivatives/mriqc
 
 subjects=(103)
 
-rm -f $logdir/cmd_mriqc_${PBS_JOBID}.txt
-touch $logdir/cmd_mriqc_${PBS_JOBID}.txt
-
-if [ ! -d $dsroot/derivatives/mriqc ]; then
-        mkdir -p $dsroot/derivatives/mriqc
-fi
-
-scratch=$dsroot/scratch
-if [ ! -d $scratch ]; then
-	mkdir -p $scratch
-fi
+rm -f $mydir/logs/cmd_mriqc_${PBS_JOBID}.txt
+touch $mydir/logs/cmd_mriqc_${PBS_JOBID}.txt
 
 TEMPLATEFLOW_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/templateflow
 MPLCONFIGDIR_DIR=/gpfs/scratch/tug87422/smithlab-shared/tools/mplconfigdir
-export APPTAINERENV_TEMPLATEFLOW_HOME=/gpfs/scratch/tug87422/smithlab-shared/tools/templateflow
-export APPTAINERENV_MPLCONFIGDIR=/gpfs/scratch/tug87422/smithlab-shared/tools/mplconfigdir
+export APPTAINERENV_TEMPLATEFLOW_HOME=$TEMPLATEFLOW_DIR
+export APPTAINERENV_MPLCONFIGDIR=$MPLCONFIGDIR_DIR
 
-export job_scratch=$scratch/mriqc_${PBS_JOBID}
+export job_scratch=$mydir/scratch/mriqc_${PBS_JOBID}
 mkdir -p $job_scratch
 
 for sub in ${subjects[@]}; do
@@ -46,7 +41,7 @@ for sub in ${subjects[@]}; do
         -B ${TEMPLATEFLOW_DIR}:/templateflow \
 	-B ${MPLCONFIGDIR_DIR}:/mplconfigdir \
         -B $dsroot/bids:/data \
-        -B $dsroot/derivatives/mriqc:/out \
+        -B $mydir/derivatives/mriqc:/out \
         -B $job_scratch:/workdir \
         /gpfs/scratch/tug87422/smithlab-shared/tools/mriqc-24.0.2.simg \
         /data /out participant \
@@ -54,7 +49,7 @@ for sub in ${subjects[@]}; do
 	--no-datalad-get \
 	--no-sub \
 	--modalities T1w \
-        -w /workdir >> $logdir/cmd_mriqc_${PBS_JOBID}.txt
+        -w /workdir >> $mydir/logs/cmd_mriqc_${PBS_JOBID}.txt
 done
 
-torque-launch -p $logdir/chk_mriqc_${PBS_JOBID}.txt $logdir/cmd_mriqc_${PBS_JOBID}.txt
+torque-launch -p $mydir/logs/chk_mriqc_${PBS_JOBID}.txt $mydir/logs/cmd_mriqc_${PBS_JOBID}.txt
